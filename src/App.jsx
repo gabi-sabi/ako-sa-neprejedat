6d332941dfb0b8168b3aa7eb86bed8baca4f44d1
unknown <gsabolova5@gmail.com>
2024-05-20 17:51:54 +0200

    Add test header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
+import { Header } from "./components/Header"
 import { Button } from "./components/ui/Button"
+
+
 
 export const App = () => {
   return (
     <>
+      <Header />
       <Button>Hello Button</Button>
     </>
   )
