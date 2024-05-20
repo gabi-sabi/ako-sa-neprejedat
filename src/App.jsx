57fdc41509886d99ba3b337023c4ec710994da01
unknown <gsabolova5@gmail.com>
2024-05-20 16:32:51 +0200

    Config tailwind for js and add test button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './App.css'
-import { Header } from "./components/Header"
+import { Button } from "./components/ui/Button"
 
 export const App = () => {
   return (
-      <Header />
+    <>
+      <Button>Hello Button</Button>
+    </>
   )
 }
