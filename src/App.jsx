c6bb05bb3f5f97a7b247bab328ab7ea6316289aa
Gabriela Sabolová <147968160+gabi-sabi@users.noreply.github.com>
2024-05-21 15:06:47 +0200

    Fix tailwind setup (#4)
    * Setup tailwind
    
    * Fix tailwind setup
    
    * Config tailwind for js and add test button
    
    * Add test header
    
    * Add test header
    
    * Add test Footer
    
    * Restored package-lock.json file
    
    * "Add files after reset"
    
    * "Commit changes after reset"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import './App.css'
 import { Header } from "./components/Header"
 import { Button } from "./components/ui/Button"
-
-
+import { Footer } from './components/Footer'
 
 export const App = () => {
   return (
     <>
       <Header />
-      <Button>Hello Button</Button>
+      <div className='container'>
+        <Button className>Button</Button>
+      </div>
+      <Footer />
     </>
   )
 }
