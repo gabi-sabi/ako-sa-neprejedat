a30d6aaab3c01306abcc990ad3b458ca72a16d1c
Jan Kuzelik <jan.kuzelik@apify.com>
2024-05-26 11:11:19 +0200

    Pair programming progress

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,18 @@ export const App = () => {
     <>
       <div className="flex flex-col h-screen">
         <Header />
-        <main className=" flex-1 flex-col items-center justify-center font-sans h-auto text-center">
-          <div className="items-center justify-center h-40">
-            <div className="flex items-center justify-center h-auto">
-              <div className="flex-col items-center justify-center h-auto">
-                <h1 className="text-white text-5xl text-center">
+        <main className="flex flex-1 flex-col items-center justify-center font-sans h-auto">
+          <div className="flex flex-row items-center justify-center gap-4">
+            <div className="flex flex-col items-center">
+                <h1 className="text-white text-5xl">
                   <span className="font-thin break-all">AKO SA PRESTAŤ</span>{" "}
                   <span className="font-bold">PREJEDAŤ?</span>
                 </h1>
-              </div>
-              <div className="flex-col items-center justify-center h-auto">
-                <h2 className="text-white text-5xl font-thin text-center">
-                  s nami!
-                </h2>
-              </div>
+              <h2 className="text-white text-5xl font-thin">
+                s nami!
+              </h2>
             </div>
-            <div className="flex items-center justify-center h-16 text-center">
+            <div className="flex flex-col items-center justify-center gap-4">
               <Button>Prvá pomoc</Button>
               <Button>Podporná skupina</Button>
               <Button>Konzultácia</Button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,108 +1,82 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import PropTypes from "prop-types";
+
+const HeaderItem = ({ href, children }) => {
+  return (
+    <li>
+      <a
+        href={href}
+        className="font-medium py-2 px-3 text-white hover:text-blue-700 md:p-0"
+      >
+        {children}
+      </a>
+    </li>
+  );
+}
+
+HeaderItem.propTypes = {
+  href: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+const AvatarButton = () => {
+  return (
+    <button
+      type="button"
+      className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 "
+      id="user-menu-button"
+      aria-expanded="false"
+      data-dropdown-toggle="user-dropdown"
+      data-dropdown-placement="bottom"
+    >
+      <span className="sr-only">Open user menu</span>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </button>
+  );
+}
+
+const headerItems = [
+  {
+    href: "#",
+    text: "Prvá pomoc",
+  },
+  {
+    href: "#",
+    text: "Podporná skupina",
+  },
+  {
+    href: "#",
+    text: "Konzultácia",
+  },
+  {
+    href: "#",
+    text: "Hry",
+  },
+];
 
 export const Header = () => {
   return (
-    <nav className="bg-transparent text-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-transparent text-white">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Ako sa neprejedať
-          </span>
+        <a href="#" className="text-2xl font-semibold whitespace-nowrap">
+          Ako sa neprejedať
         </a>
-        <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button
-            type="button"
-            className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-            id="user-menu-button"
-            aria-expanded="false"
-            data-dropdown-toggle="user-dropdown"
-            data-dropdown-placement="bottom"
-          >
-            <span className="sr-only">Open user menu</span>
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-          </button>
-          <div
-            className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-            id="user-dropdown"
-          >
-            <div className="px-4 py-3">
-              <span className="block text-sm text-gray-900 dark:text-white">
-                Jana Nováková
-              </span>
-              <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
-                novakovaj@domena.sk
-              </span>
-            </div>
-            <ul className="py-2" aria-labelledby="user-menu-button">
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Moje nástenka
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Moje rezervace
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >
-                  Odhlásiť sa
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-user"
-        >
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-transparent md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+        <div className="items-center justify-between flex w-auto">
+          <ul className="flex p-4 md:p-0 bg-transparent md:space-x-8">
+            {headerItems.map((item, index) => (
+              <HeaderItem 
+                key={index}
+                href={item.href}                
               >
-                Prvá pomoc
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Podporná skupina
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Konzultácia
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Hry
-              </a>
-            </li>
+                {item.text}
+              </HeaderItem>
+            ))}
           </ul>
         </div>
+        <AvatarButton />
       </div>
     </nav>
   );
