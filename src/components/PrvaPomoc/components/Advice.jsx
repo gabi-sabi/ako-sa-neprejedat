3e7fc35614116975bc3675f2481936ff14d6b6cb
Gabriela Sabolova <gsabolova5@gmail.com>
2024-05-27 20:23:04 +0200

    Fix styles of list in Advice

--- src/components/PrvaPomoc/components/Advice.jsx
+++ src/components/PrvaPomoc/components/Advice.jsx
@@ -1,4 +1,5 @@
 import { X } from "lucide-react";
+import { Smile } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 
@@ -10,7 +11,7 @@ export const Advice = ({ type, area }) => {
       {isVisible && (
         <div className="flex w-full flex-row items-stretch">
           {type === "respFyzio" ? (
-            <button className="cursor-pointer relative flex w-full flex-row justify-center items-center bg-white my-5 rounded-xl p-5">
+            <button className="cursor-pointer relative flex w-full flex-col justify-center items-center bg-white my-5 rounded-xl p-5">
               {area.adviceFyzio}
               <div className="absolute top-2 right-2">
                 <Button
@@ -22,12 +23,19 @@ export const Advice = ({ type, area }) => {
                   <X className="h-4 w-4" />
                 </Button>
               </div>
+              <div className="py-5"><Smile color="#A5DD9B" className="w-10 h-10"/></div>
             </button>
+            
           ) : (
-            <button className="cursor-pointer relative flex w-full flex-row justify-center items-center bg-white my-5 rounded-xl p-5">
+            <button className="cursor-pointer relative flex w-full flex-col justify-center items-center bg-white my-5 rounded-xl px-10 py-5">
               <ul>
-                {area.adviceEmot.map((step, index) => (
-                  <li key={index}>{step}</li>
+                {area.adviceEmot.title.map((step, index) => (
+                  <li className="text-left" key={index}>{step}</li>
+                ))}
+              </ul>
+              <ul>
+                {area.adviceEmot.points.map((step, index) => (
+                  <li className="list-disc text-left" key={index}>{step}</li>
                 ))}
               </ul>
               <div className="absolute top-2 right-2">
