cf991fbfcda6dca607974ed5664f506c2826b7b5
Gabriela Sabolova <gsabolova5@gmail.com>
2024-05-28 20:34:48 +0200

    Fix proptypes

--- src/components/PrvaPomoc/components/Advice.jsx
+++ src/components/PrvaPomoc/components/Advice.jsx
@@ -1,8 +1,9 @@
 import { X } from "lucide-react";
+import { Smile } from 'lucide-react';
 import { Button } from "@/components/ui/button";
 import React, { useState } from "react";
 
-export const Advice = ({ type, area }) => {
+export const Advice = ({ type, area, setResponseType }) => {
   const [isVisible, setIsVisible] = useState(true);
 
   return (
@@ -10,24 +11,34 @@ export const Advice = ({ type, area }) => {
       {isVisible && (
         <div className="flex w-full flex-row items-stretch">
           {type === "respFyzio" ? (
-            <button className="cursor-pointer relative flex w-full flex-row justify-center items-center bg-white my-5 rounded-xl p-5">
+            <button className="cursor-pointer relative flex w-full flex-col justify-center items-center bg-white my-5 rounded-xl p-5">
               {area.adviceFyzio}
               <div className="absolute top-2 right-2">
                 <Button
                   variant="outline"
                   size="h-10 w-10"
                   className="flex"
-                  onClick={() => setIsVisible(!isVisible)}
+                  onClick={() => {
+                    setIsVisible(!isVisible)
+                    setResponseType(null)
+                  }}    
                 >
                   <X className="h-4 w-4" />
                 </Button>
               </div>
+              <div className="py-5"><Smile color="#A5DD9B" className="w-10 h-10"/></div>
             </button>
+            
           ) : (
-            <button className="cursor-pointer relative flex w-full flex-row justify-center items-center bg-white my-5 rounded-xl p-5">
+            <button className="cursor-pointer relative flex w-full flex-col justify-center items-center bg-white my-5 rounded-xl px-10 py-5">
               <ul>
-                {area.adviceEmot.map((step, index) => (
-                  <li key={index}>{step}</li>
+                {area.adviceEmot.title.map((step, index) => (
+                  <li className="text-left" key={index}>{step}</li>
+                ))}
+              </ul>
+              <ul>
+                {area.adviceEmot.points.map((step, index) => (
+                  <li className="list-disc text-left" key={index}>{step}</li>
                 ))}
               </ul>
               <div className="absolute top-2 right-2">
--- src/components/PrvaPomoc/components/Question.jsx
+++ src/components/PrvaPomoc/components/Question.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Advice } from "./Advice";
 import { useState } from "react";
 
