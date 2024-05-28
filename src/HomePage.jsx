d573fe6a4fef5f57841412c158a2b81886095ad3
Gabriela Sabolova <gsabolova5@gmail.com>
2024-05-28 12:00:57 +0200

    Add same width to buttons and style to title

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -12,16 +12,18 @@ export const HomePage = () => {
         <Header />
         <main className="flex flex-1 flex-col items-center justify-center font-sans h-auto">
           <div className="flex flex-row items-center justify-center gap-4">
-            <div className="flex flex-col items-center">
-              <h1 className="text-white text-5xl">
+            <div className=" w-1/2 flex flex-col items-center gap-20">
+              <h1 className="text-white text-center text-5xl">
                 <span className="font-thin break-all">AKO SA PRESTAŤ</span>{" "}
-                <span className="font-bold">PREJEDAŤ?</span>
+                <span className="font-black">PREJEDAŤ?</span>
               </h1>
-              <h2 className="text-white text-5xl font-thin">s nami!</h2>
+              <h2 className="text-white text-5xl font-thin italic">s nami!</h2>
             </div>
-            <div className="flex flex-col items-center justify-center gap-4">
+            <div className=" w-1/2 grid grid-cols-1  justify-items-center">
+              <div className="w-1/2 grid gap-4">
               <Link to="/prvapomoc">
-                <ButtonBasic color="light-red">Prvá pomoc</ButtonBasic>
+                <ButtonBasic size="lg" color="light-red"
+                >Prvá pomoc</ButtonBasic>
               </Link>
               <Link to="/skupina">
                 <ButtonBasic color="light-brown">Podporná skupina</ButtonBasic>
@@ -32,6 +34,7 @@ export const HomePage = () => {
               <Link to="/hry">
                 <ButtonBasic color="light-yellow">Hry</ButtonBasic>
               </Link>
+              </div>
             </div>
           </div>
         </main>
--- src/components/ButtonBasic.jsx
+++ src/components/ButtonBasic.jsx
@@ -1,12 +1,12 @@
 import { Button } from "@/components/ui/button";
 
-export const ButtonBasic = ({ onClick, link, children, color }) => {
+export const ButtonBasic = ({ onClick, link, children, color, size }) => {
   return (
     <Button
       onClick={onClick}
       link={link}
       variant="secondary"
-      className={`w-full bg-${color} hover:bg-${color}/90`}
+      className={`w-full bg-${color} hover:bg-${color}/90 size-${size}`}
     >
       <div className="text-white font-bold font-2xl">
       {children}
