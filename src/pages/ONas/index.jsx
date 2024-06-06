69121b3d7540450741af6158ffbf471d8cc6e756
Alice Veselá <alice.hladikova@gmail.com>
2024-06-06 21:27:17 +0200

    processed feedback AboutUsComponent

--- src/pages/ONas/index.jsx
+++ src/pages/ONas/index.jsx
@@ -4,39 +4,37 @@ import skupina_about_cut from '../../assets/skupina_about_cut.jpg';
 
 export const ONasPage = () => {
   return (
-    <>
-      <div className="flex flex-row justify-center gap-4">
-        <AboutUs
-          name="Gabriela Sabolova"
-          img={konz_about_cut}
-          email="dietologicka@dietologicka.eu"
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
-            malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
-            odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
-            Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus.
-            Aenean fermentum felis at ex dignissim, id scelerisque tortor
-            vehicula. Phasellus aliquet nunc ut ante tinc
-          </p>
-        </AboutUs>
-        <AboutUs
-          name="Gabriela Sabolova"
-          img={skupina_about_cut}
-          email="gsabolova5@gmail.com"
-        >
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
-            malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
-            odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
-            Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus.
-            Aenean fermentum felis at ex dignissim, id scelerisque tortor
-            vehicula. Phasellus aliquet nunc ut ante tinc
-          </p>
-        </AboutUs>
-      </div>
-    </>
+    <div className="flex flex-row justify-center gap-4">
+      <AboutUs
+        name="Gabriela Sabolova"
+        img={konz_about_cut}
+        email="dietologicka@dietologicka.eu"
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
+          malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
+          odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
+          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean
+          fermentum felis at ex dignissim, id scelerisque tortor vehicula.
+          Phasellus aliquet nunc ut ante tinc
+        </p>
+      </AboutUs>
+      <AboutUs
+        name="Gabriela Sabolova"
+        img={skupina_about_cut}
+        email="gsabolova5@gmail.com"
+      >
+        <p className="leading-normal ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
+          malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
+          odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
+          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean
+          fermentum felis at ex dignissim, id scelerisque tortor vehicula.
+          Phasellus aliquet nunc ut ante tinc
+        </p>
+      </AboutUs>
+    </div>
   );
 };
