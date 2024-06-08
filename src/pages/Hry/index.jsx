8aeb03efa07cc456031637c0d13459a7a2ecadc3
Jan Kuzelik <jan.kuzelik@apify.com>
2024-06-08 11:35:14 +0200

    Local GameButton component

--- src/pages/Hry/index.jsx
+++ src/pages/Hry/index.jsx
@@ -1,19 +1,30 @@
-import { GameButton } from '@/components/GameButton';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const GameButton = ({ to, children }) => {
+  return (
+    <Link
+      to={to}
+      className="bg-white text-center rounded-lg text-light-brown w-full py-8 hover:bg-slate-100 text-4xl font-bold"
+    >
+      {children}
+    </Link>
+  )
+}
+
+GameButton.propTypes = {
+  to: PropTypes.string,
+  children: PropTypes.node,
+}
 
 export const HryPage = () => {
   return (
     <>
-      <main className="flex flex-col items-center justify-center font-sans h-auto">
-        <GameButton to="/dychacie-cvicenie" name="dychacie cvicenie">
-          dychacie cvicenie
-        </GameButton>
-        <GameButton to="/pocitanie" name="pocitanie">
-          pocitanie
-        </GameButton>
-        <GameButton to="/rozdiely" name="rozdiely">
-          rozdiely
-        </GameButton>
+      <main className="flex flex-col items-start justify-center font-sans h-auto gap-4 px-12 md:px-32 lg:px-64">
+        <GameButton to="/dychacie-cvicenie">Dychacie cvicenie</GameButton>
+        <GameButton to="/pocitanie">Pocitanie</GameButton>
+        <GameButton to="/rozdiely">Rozdiely</GameButton>
       </main>
     </>
-  );
-};
+  )
+}
