560beb91f63dc88d57e7afdb3c0594b525a7fd3b
Alice Veselá <alice.hladikova@gmail.com>
2024-06-08 14:42:01 +0200

    diferences game

--- client/src/pages/Hry/Diferencies.jsx
+++ client/src/pages/Hry/Diferencies.jsx
@@ -1,1 +1,28 @@
-export const DiferenciesPage = () => {};
+import { useState } from 'react';
+import { gameData } from './gameData';
+import { isWithinDifferenceBoxes } from './gameLogic';
+
+export const DiferenciesPage = () => {
+  const [found, setFound] = useState([]);
+
+  const currentGameState = gameData[0];
+
+  const handleImageClick = (event) => {
+    const clickedDifference = isWithinDifferenceBoxes(
+      event.nativeEvent.offsetX,
+      event.nativeEvent.offsetY,
+      currentGameState.differences,
+    );
+
+    if (clickedDifference) {
+      setFound([...found, clickedDifference]);
+      console.log(clickedDifference);
+    }
+  };
+  return (
+    <div className="flex justify-center">
+      <img src={currentGameState.file1} onMouseDown={handleImageClick} />
+      <img src={currentGameState.file2} onMouseDown={handleImageClick} />
+    </div>
+  );
+};
---
+++ client/src/pages/Hry/gameData.js
@@ -0,0 +1,15 @@
+import rozdil1left from '@/assets/rozdil1left.jpg';
+import rozdil1right from '@/assets/rozdil1right.jpg';
+
+export const gameData = [
+  {
+    file1: rozdil1left,
+    file2: rozdil1right,
+    differences: [
+      { x: 155, y: 243, w: 100, h: 100 },
+      { x: 49, y: 426, w: 100, h: 100 },
+      { x: 276, y: 515, w: 100, h: 100 },
+      { x: 295, y: 60, w: 100, h: 100 },
+    ],
+  },
+];
