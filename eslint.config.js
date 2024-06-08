9958d437c82782d2a79cf2dae516c42efaf94109
Jkuzz <68104924+Jkuzz@users.noreply.github.com>
2024-06-08 11:55:37 +0200

    Use new eslint config (#15)

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,12 @@ import pluginJs from "@eslint/js";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
 import { fixupConfigRules } from "@eslint/compat";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...fixupConfigRules(pluginReactConfig),
   eslintConfigPrettier,
+  eslintPluginPrettierRecommended,
 ];
