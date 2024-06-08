1c174b348d9ba9385fe41291adbdbf01d63af7fa
Alice <147323150+alice-ves@users.noreply.github.com>
2024-06-08 11:49:57 +0200

    Games section page (#13)
    * about us component
    
    * creating game page
    
    * processed feedback
    
    * Local GameButton component
    
    * cleanup
    
    ---------
    
    Co-authored-by: Jan Kuzelik <jan.kuzelik@apify.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from "react-router-dom";
-import { HomePage } from "./HomePage";
-import { PrvaPomocPage } from "./pages/PrvaPomoc";
-import { SkupinaPage } from "./pages/Skupina";
-import { KonzultaciaPage } from "./pages/Konzultacia";
-import { ONasPage } from "./pages/ONas";
-import { HryPage } from "./pages/Hry";
-import { Header } from "../src/components/Header";
-import { Footer } from "../src/components/Footer";
-import { Toaster } from "react-hot-toast";
-import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { PrvaPomocPage } from './pages/PrvaPomoc';
+import { SkupinaPage } from './pages/Skupina';
+import { KonzultaciaPage } from './pages/Konzultacia';
+import { ONasPage } from './pages/ONas';
+import { HryPage } from './pages/Hry';
+import { Header } from '../src/components/Header';
+import { Footer } from '../src/components/Footer';
+import { Toaster } from 'react-hot-toast';
+import './App.css';
+import { BreathingExercisesPage } from './pages/Hry/Breathing exercises';
+import { DiferenciesPage } from './pages/Hry/Diferencies';
+import { CountingPage } from './pages/Hry/Counting';
 
 export const App = () => {
   return (
@@ -23,6 +26,12 @@ export const App = () => {
           <Route path="/konzultacia" element={<KonzultaciaPage />} />
           <Route path="/o-nas" element={<ONasPage />} />
           <Route path="/hry" element={<HryPage />} />
+          <Route
+            path="/dychacie-cvicenie"
+            element={<BreathingExercisesPage />}
+          />
+          <Route path="/rozdiely" element={<DiferenciesPage />} />
+          <Route path="/pocitanie" element={<CountingPage />} />
         </Routes>
         <Footer />
       </div>
--- src/pages/Hry/index.jsx
+++ src/pages/Hry/index.jsx
@@ -1,9 +1,30 @@
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+const GameButton = ({ to, children }) => {
+  return (
+    <Link
+      to={to}
+      className="bg-white text-center rounded-lg text-light-brown w-full py-8 hover:bg-slate-100 text-4xl font-bold"
+    >
+      {children}
+    </Link>
+  )
+}
+
+GameButton.propTypes = {
+  to: PropTypes.string,
+  children: PropTypes.node,
+}
+
 export const HryPage = () => {
   return (
     <>
-      <main className="flex flex-col items-center justify-center font-sans h-auto">
-        <h1 className="font-bold text-white text-4xl">Hry</h1>
+      <main className="flex flex-col items-start justify-center font-sans h-auto gap-4 px-12 md:px-32 lg:px-64">
+        <GameButton to="/dychacie-cvicenie">Dychacie cvicenie</GameButton>
+        <GameButton to="/pocitanie">Pocitanie</GameButton>
+        <GameButton to="/rozdiely">Rozdiely</GameButton>
       </main>
     </>
-  );
-};
+  )
+}
