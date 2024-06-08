484988f42b54a2154885f704d176f1994caf829e
Alice Veselá <alice.hladikova@gmail.com>
2024-06-08 15:27:52 +0200

    differences clickable

--- client/src/pages/Hry/Diferencies.jsx
+++ client/src/pages/Hry/Diferencies.jsx
@@ -2,6 +2,28 @@ import { useState } from 'react';
 import { gameData } from './gameData';
 import { isWithinDifferenceBoxes } from './gameLogic';
 
+const GameImage = ({ src, onMouseDown, foundDifferences }) => {
+  console.log(foundDifferences);
+  return (
+    <div className="relative">
+      <img src={src} onMouseDown={onMouseDown} />
+
+      {foundDifferences?.map((difference) => (
+        <div
+          key={`${difference.x}-${difference.y}`}
+          className="bg-light-green absolute bg-opacity-50 border border-green-700 rounded-lg"
+          style={{
+            width: `${difference.w}px`,
+            height: `${difference.h}px`,
+            top: `${difference.y}px`,
+            left: `${difference.x}px`,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+};
+
 export const DiferenciesPage = () => {
   const [found, setFound] = useState([]);
 
@@ -21,8 +43,12 @@ export const DiferenciesPage = () => {
   };
   return (
     <div className="flex justify-center">
-      <img src={currentGameState.file1} onMouseDown={handleImageClick} />
-      <img src={currentGameState.file2} onMouseDown={handleImageClick} />
+      <GameImage
+        src={currentGameState.file1}
+        onMouseDown={handleImageClick}
+        foundDifferences={found}
+      />
+      <GameImage src={currentGameState.file2} onMouseDown={handleImageClick} />
     </div>
   );
 };
