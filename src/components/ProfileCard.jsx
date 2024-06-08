4265cd6473fb2bafb8e6747b63b4bd93b5cad6a8
Alice <147323150+alice-ves@users.noreply.github.com>
2024-06-08 10:43:34 +0200

    about us component (#12)
    * about us component
    
    * processed feedback AboutUsComponent
    
    * edit prop-types
    
    * fix merge

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+export const AboutUs = ({ img, name, email, children }) => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center w-4/12 p-6">
+        <h2 className="text-bold text-center text-4xl">{name}</h2>
+        <img
+          src={img}
+          alt={`ProfilePicture${name}`}
+          className="rounded-full h-48 m-4"
+        />
+        {children}
+        <span className="font-bold m-8">
+          E-mail:
+          <a className="underline" href={`mailto:${email}`}>
+            {email}
+          </a>
+        </span>
+      </div>
+    </>
+  );
+};
+AboutUs.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+  email: PropTypes.string,
+  children: PropTypes.node,
+};
--- src/pages/ONas/index.jsx
+++ src/pages/ONas/index.jsx
@@ -1,25 +1,40 @@
-import konz_about_cut from "../../assets/konz_about_cut.jpg";
-import skupina_about_cut from "../../assets/skupina_about_cut.jpg";
+import { AboutUs } from '@/components/ProfileCard';
+import konz_about_cut from '../../assets/konz_about_cut.jpg';
+import skupina_about_cut from '../../assets/skupina_about_cut.jpg';
 
 export const ONasPage = () => {
   return (
-    <>
-      <div className="flex flex-row gap-8 py-20 px-60 items-center justify-center">
-        <div className="flex w-1/2 items-center justify-center">
-          <img
-            src={skupina_about_cut}
-            alt="ProfilePicture"
-            className="rounded-full h-48"
-          />
-        </div>
-        <div className="flex w-1/2 items-center justify-center">
-          <img
-            src={konz_about_cut}
-            alt="ProfilePicture"
-            className="rounded-full h-48"
-          />
-        </div>
-      </div>
-    </>
+    <div className="flex flex-row justify-center gap-4">
+      <AboutUs
+        name="Gabriela Sabolova"
+        img={konz_about_cut}
+        email="dietologicka@dietologicka.eu"
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
+          malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
+          odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
+          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean
+          fermentum felis at ex dignissim, id scelerisque tortor vehicula.
+          Phasellus aliquet nunc ut ante tinc
+        </p>
+      </AboutUs>
+      <AboutUs
+        name="Gabriela Sabolova"
+        img={skupina_about_cut}
+        email="gsabolova5@gmail.com"
+      >
+        <p className="leading-normal ">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
+          malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
+          odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
+          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean
+          fermentum felis at ex dignissim, id scelerisque tortor vehicula.
+          Phasellus aliquet nunc ut ante tinc
+        </p>
+      </AboutUs>
+    </div>
   );
 };
