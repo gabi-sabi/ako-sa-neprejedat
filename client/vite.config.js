930b278f110c428c5d81cb618806b2c07b5bf60b
Jan Kuzelik <jan.kuzelik@apify.com>
2024-06-09 10:56:07 +0200

    Add docker files

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,28 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
     port: 5173, // Vite's port
+    strictPort: true,
     proxy: {
-        '/api': {
-            target: 'http://localhost:3500', // Express server's port
-            changeOrigin: true,
-            secure: false,
-        }
-    }
-}
-});
+      '/api': {
+        target: 'http://localhost:3500', // Express server's port
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+})
