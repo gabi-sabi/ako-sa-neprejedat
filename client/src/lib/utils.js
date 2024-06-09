37f478be06eb4f0508d582cd8ca44c8fdcf30efc
Katerina Hronikova <katacek@gmail.com>
2024-06-09 17:17:53 +0200

    adding callbackend with env fcn

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -1,11 +1,13 @@
 import { clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import dayjs from 'dayjs';
+import axios from "axios";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
+// get next four timeslots for Monday and Thursday at 19:00
 export const getNextFourTimeslots = () => {
   const timeslots = [];
   const today = dayjs();
@@ -32,3 +34,29 @@ export const getNextFourTimeslots = () => {
 
   return timeslots;
 };
+
+export async function callBackend({ method = 'POST', url, payload, headers}) {
+  const backendUrl = import.meta.env.BACKEND_BASE_URL ? `${import.meta.env.BACKEND_BASE_URL}/${url}` : `http://localhost:3500/${url}`
+  try {
+    const { data, status } = await axios({
+      method,
+      url: backendUrl,
+      data: payload,
+      headers: headers || { "Content-Type": "application/json" }
+    });
+    // back-end returns response {} with data {}, in those data, there is the message which is {data: string}, thus response.data
+    if (status != 200 && status != 201)
+    {
+      throw new Error(`response status is ${status}`);
+    }
+    return data;
+  }
+  
+  catch (error)
+  {
+    return {
+      error: true,
+      message: error.message
+    }
+  }
+}
