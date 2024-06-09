8bd53e7e1e68400931baf2f7079ae2afbc1280ed
Jan Kuzelik <jan.kuzelik@apify.com>
2024-06-09 16:15:14 +0200

    Format

--- client/src/pages/ONas/index.jsx
+++ client/src/pages/ONas/index.jsx
@@ -1,6 +1,6 @@
-import { AboutUs } from '@/components/ProfileCard';
-import konz_about_cut from '../../assets/konz_about_cut.jpg';
-import skupina_about_cut from '../../assets/skupina_about_cut.jpg';
+import { AboutUs } from '@/components/ProfileCard'
+import konz_about_cut from '../../assets/konz_about_cut.jpg'
+import skupina_about_cut from '../../assets/skupina_about_cut.jpg'
 
 export const ONasPage = () => {
   return (
@@ -11,13 +11,11 @@ export const ONasPage = () => {
         email="dietologicka@dietologicka.eu"
       >
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
-          malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
-          odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
-          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean
-          fermentum felis at ex dignissim, id scelerisque tortor vehicula.
-          Phasellus aliquet nunc ut ante tinc
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae
+          vestibulum vestibulum. Cras venenatis euismod malesuada. Nulla facilisi. Duis ornare, quam
+          eu efficitur vehicula, odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
+          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean fermentum felis at
+          ex dignissim, id scelerisque tortor vehicula. Phasellus aliquet nunc ut ante tinc
         </p>
       </AboutUs>
       <AboutUs
@@ -26,15 +24,13 @@ export const ONasPage = () => {
         email="gsabolova5@gmail.com"
       >
         <p className="leading-normal ">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          lacinia odio vitae vestibulum vestibulum. Cras venenatis euismod
-          malesuada. Nulla facilisi. Duis ornare, quam eu efficitur vehicula,
-          odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
-          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean
-          fermentum felis at ex dignissim, id scelerisque tortor vehicula.
-          Phasellus aliquet nunc ut ante tinc
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae
+          vestibulum vestibulum. Cras venenatis euismod malesuada. Nulla facilisi. Duis ornare, quam
+          eu efficitur vehicula, odio lacus tincidunt eros, non tincidunt eros nisl sit amet eros.
+          Mauris auctor eros nec quam dapibus, ut ultricies nisi luctus. Aenean fermentum felis at
+          ex dignissim, id scelerisque tortor vehicula. Phasellus aliquet nunc ut ante tinc test
         </p>
       </AboutUs>
     </div>
-  );
-};
+  )
+}
