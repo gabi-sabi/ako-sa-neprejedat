68d740d59e97007b4fe5231e4ba17d0288bab371
Alice <147323150+alice-ves@users.noreply.github.com>
2024-06-16 17:49:48 +0200

    Dychaci cviceni (#31)
    * Adding Simple Arithmetic
    
    * breathing exercise to fix
    
    * breathing exercise breathes
    
    * bubble component removed

--- client/src/pages/Hry/Breathing exercises.jsx
+++ client/src/pages/Hry/Breathing exercises.jsx
@@ -1,1 +1,65 @@
-export const BreathingExercisesPage = () => {};
+import React, { useState, useEffect } from 'react';
+
+export const BreathingExercisesPage = () => {
+  const [number, setNumber] = useState(1);
+  const [isExpanding, setIsExpanding] = useState(true);
+  const [size, setSize] = useState(200);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setNumber((previousNumber) => {
+        if (isExpanding) {
+          if (previousNumber < 5) {
+            setSize((previousSize) => previousSize + 40);
+            return previousNumber + 1;
+          } else {
+            setIsExpanding(false);
+            return previousNumber;
+          }
+        } else {
+          if (previousNumber > 1) {
+            setSize((previousSize) => previousSize - 40);
+            return previousNumber - 1;
+          } else {
+            setIsExpanding(true);
+            return previousNumber;
+          }
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [isExpanding]);
+
+  const circleStyle = {
+    width: `${size}px`,
+    height: `${size}px`,
+    borderRadius: '50%',
+    backgroundColor: '#A5DD9B',
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: '4rem',
+    transition: 'width 1s, height 1s',
+  };
+
+  return (
+    <div className="bg-white flex flex-col justify-center items-center relative">
+      <p className="absolute top-6 text-lg color-green-900">
+        Vykonajte toto jednoduché cvičenie dýchania:
+        <br />
+        Sledujte objekt, ako sa zväčšuje a zmenšuje. Keď sa objekt zväčšuje,
+        dýchnite pomaly a hlboko. <br />
+        Keď sa objekt zmenšuje, vydychujte pomaly a úplne.
+        <br /> Opakujte tento cyklus sústredene a pokojne.
+      </p>
+      <div style={circleStyle}>{number}</div>
+
+      <div className="w-40 h-40 rounded-full bg-light-orange opacity-80 shadow-2xl absolute inset-40 "></div>
+      <div className="w-80 h-80 rounded-full bg-dark-blue opacity-80 shadow-2xl absolute right-20 bottom-20"></div>
+      <div className="w-60 h-60 rounded-full bg-light-yellow opacity-80 shadow-2xl absolute left-80  bottom-20"></div>
+      <div className="w-60 h-60 rounded-full bg-light-red opacity-80 shadow-2xl absolute right-80  top-20"></div>
+    </div>
+  );
+};
--- client/src/pages/Hry/Counting.jsx
+++ client/src/pages/Hry/Counting.jsx
@@ -1,1 +1,67 @@
-export const CountingPage = () => {};
+import { useEffect, useState } from 'react';
+
+export const CountingPage = () => {
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [secondNumber, setSecondNumber] = useState(null);
+  const [userAnswer, setUserAnswer] = useState('');
+  const [message, setMessage] = useState('');
+
+  const generateNumbers = () => {
+    setFirstNumber(Math.round(Math.random() * 20));
+    setSecondNumber(Math.round(Math.random() * 20));
+    setUserAnswer('');
+  };
+
+  useEffect(() => {
+    generateNumbers();
+  }, []);
+  console.log(firstNumber, secondNumber);
+
+  const checkResult = (e) => {
+    e.preventDefault();
+    const userAnswerNum = Number(userAnswer);
+    if (userAnswerNum === firstNumber + secondNumber) {
+      setMessage('spravne');
+      setTimeout(() => {
+        generateNumbers();
+        setMessage('');
+      }, 2000);
+    } else {
+      setMessage('Zkus to jeste jednou');
+      setUserAnswer('');
+    }
+  };
+
+  return (
+    <div className="bg-white flex flex-col justify-center items-center text-8xl ">
+      <form className="flex flex-col items-center" onSubmit={checkResult}>
+        <label className="text-green-900">
+          {firstNumber}+{secondNumber}=
+          <input
+            type="number"
+            className="bg-light-green bg-opacity-25 rounded w-40 outline-none"
+            value={userAnswer}
+            onChange={(event) => {
+              setUserAnswer(event.target.value);
+            }}
+          ></input>
+        </label>
+        <button className="text-base font-bold text-green-900 m-12 bg-light-orange p-2 m-12">
+          zkontroluj
+        </button>
+      </form>
+      <p
+        className={`text-2l font-bold text-center fixed bottom-40
+           ${
+             message === 'spravne'
+               ? 'text-light-green'
+               : message === 'Zkus to jeste jednou'
+                 ? 'text-light-red'
+                 : ''
+           }`}
+      >
+        {message}
+      </p>
+    </div>
+  );
+};
